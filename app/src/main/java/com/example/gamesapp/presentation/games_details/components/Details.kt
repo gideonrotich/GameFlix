a6b4f86ba0d05a278b95e5da5ecf40bdb0a1b11f
Giddyrotich <giddyrotich433@gmail.com>
2022-09-06 11:13:44 +0300

    f

--- app/src/main/java/com/example/gamesapp/presentation/games_details/components/Details.kt
+++ app/src/main/java/com/example/gamesapp/presentation/games_details/components/Details.kt
@@ -59,7 +59,7 @@ fun Details(navController: NavController,viewModel: GamesDetailViewModel = hiltV
                         .background(color = colorResource(id = R.color.white))){
 
                         item {
-                            val image: Painter = rememberImagePainter(data = games!!.background_image)
+                            val image: Painter = rememberImagePainter(data = games?.background_image)
                             Image(
                                 modifier = Modifier
                                     .fillMaxWidth()
@@ -70,55 +70,63 @@ fun Details(navController: NavController,viewModel: GamesDetailViewModel = hiltV
                                 contentScale = ContentScale.Crop
                             )
                             Spacer(modifier = Modifier.height(16.dp))
-                            inneddetails(games!!.name, games.metacritic.toString(), games.name_original)
-                        }
-
-                        item {
-                            Spacer(modifier = Modifier.height(24.dp))
-                            Text(
-                                text = "Description",
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(16.dp, 0.dp, 16.dp, 0.dp),
-                                color = colorResource(id = R.color.text),
-                                style = MaterialTheme.typography.body2,
-                                textAlign = TextAlign.Start
-                            )
-                            Spacer(modifier = Modifier.height(16.dp))
-                            Text(
-                                text = games!!.description,
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(16.dp, 0.dp, 16.dp, 0.dp),
-                                color = colorResource(id = R.color.text),
-                                style = MaterialTheme.typography.body2,
-                                textAlign = TextAlign.Start
-                            )
-                        }
-
-                        item {
-                            Spacer(modifier = Modifier.height(24.dp))
-                            Text(
-                                text = "Game Info",
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(16.dp, 0.dp, 16.dp, 0.dp),
-                                color = colorResource(id = R.color.text),
-                                style = MaterialTheme.typography.body2,
-                                textAlign = TextAlign.Start
-                            )
-                            Spacer(modifier = Modifier.height(16.dp))
-                            Row(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(16.dp, 0.dp, 16.dp, 0.dp),
-                                horizontalArrangement = Arrangement.SpaceBetween
-                            ) {
-                                InfoCard(title = "Youtube count", value = games!!.youtube_count.toString())
-                                InfoCard(title = "Achievements", value = games!!.parent_achievements_count)
-                                InfoCard(title = "Ratings", value = games!!.metacritic.toString())
+                            if (games != null) {
+                                games.name?.let { it1 -> games.name_original?.let { it2 ->
+                                    inneddetails(it1, games.metacritic.toString(),
+                                        it2
+                                    )
+                                } }
                             }
                         }
+
+//                        item {
+//                            Spacer(modifier = Modifier.height(24.dp))
+//                            Text(
+//                                text = "Description",
+//                                modifier = Modifier
+//                                    .fillMaxWidth()
+//                                    .padding(16.dp, 0.dp, 16.dp, 0.dp),
+//                                color = colorResource(id = R.color.text),
+//                                style = MaterialTheme.typography.body2,
+//                                textAlign = TextAlign.Start
+//                            )
+//                            Spacer(modifier = Modifier.height(16.dp))
+//                            games!!.description?.let { it1 ->
+//                                Text(
+//                                    text = it1,
+//                                    modifier = Modifier
+//                                        .fillMaxWidth()
+//                                        .padding(16.dp, 0.dp, 16.dp, 0.dp),
+//                                    color = colorResource(id = R.color.text),
+//                                    style = MaterialTheme.typography.body2,
+//                                    textAlign = TextAlign.Start
+//                                )
+//                            }
+//                        }
+
+//                        item {
+//                            Spacer(modifier = Modifier.height(24.dp))
+//                            Text(
+//                                text = "Game Info",
+//                                modifier = Modifier
+//                                    .fillMaxWidth()
+//                                    .padding(16.dp, 0.dp, 16.dp, 0.dp),
+//                                color = colorResource(id = R.color.text),
+//                                style = MaterialTheme.typography.body2,
+//                                textAlign = TextAlign.Start
+//                            )
+//                            Spacer(modifier = Modifier.height(16.dp))
+//                            Row(
+//                                modifier = Modifier
+//                                    .fillMaxWidth()
+//                                    .padding(16.dp, 0.dp, 16.dp, 0.dp),
+//                                horizontalArrangement = Arrangement.SpaceBetween
+//                            ) {
+//                                InfoCard(title = "Youtube count", value = games!!.youtube_count.toString())
+//                                games!!.parent_achievements_count?.let { it1 -> InfoCard(title = "Achievements", value = it1) }
+//                                InfoCard(title = "Ratings", value = games!!.metacritic.toString())
+//                            }
+//                        }
 
                     }
                 }
